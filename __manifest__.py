e1d77b3359e5794c3123ed633c5a42f3b0670dec
piyo02 <alzidni2000@gmail.com>
2020-09-18 21:39:47 +0800

    push

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': 'Template Addon',
+    'name': 'Purchase Fixed Discount',
     'author': 'technoindo.com',
     'category': 'hidden',
     'version': '10.0',
     'summary': 'Summary the addon.',
     'description': '''Description the addon'''
                    ,
-    'depends': ['base'],
+    'depends': ['purchase', 'account_invoice_po_fixed_discount'], #
     'data': [
-        ''
+        'views/purchase_view.xml',
     ],
     'images': [''],
     'auto_install': False,
