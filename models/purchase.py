db87228706ceb89da86cf7442771c7f9e3647c89
piyo02 <alzidni2000@gmail.com>
2020-10-13 16:11:44 +0800

    push

--- models/purchase.py
+++ models/purchase.py
@@ -5,9 +5,12 @@ from odoo.exceptions import ValidationError
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    amount_discount_fixed = fields.Monetary(string='Total Discount', store=True, readonly=True, compute='_amount_all')
+
     @api.depends('order_line.price_total')
     def _amount_all(self):
         vals = {}
+        amount_discount_fixed = 0
         for line in self.mapped('order_line').filtered(
                 lambda l: l.discount_fixed):
             vals[line] = {
@@ -15,9 +18,13 @@ class PurchaseOrder(models.Model):
                 'discount_fixed': line.discount_fixed,
             }
             price_unit = line.price_unit - line.discount_fixed
+            amount_discount_fixed += line.discount_fixed*line.product_qty
             line.update({
                 'price_unit': price_unit,
                 'discount_fixed': 0.0,
+            })
+            line.order_id.update({
+                'amount_discount_fixed': amount_discount_fixed
             })
         res = super(PurchaseOrder, self)._amount_all()
         for line in vals.keys():
@@ -48,8 +55,10 @@ class PurchaseOrderLine(models.Model):
             self.discount3 = 0.0
 
     @api.onchange('discount', 'discount2', 'discount3')
+    # @api.onchange('discount')
     def _onchange_discount(self):
         if self.discount or self.discount2 or self.discount3:
+        # if self.discount:
             self.discount_fixed = 0.0
 
     @api.one
